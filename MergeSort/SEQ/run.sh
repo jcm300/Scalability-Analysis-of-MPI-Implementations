27db2d1de540099bd68add5b2105ee1f318bf3ea
MQuaresma <miguelquaresma97@gmail.com>
2019-05-28 20:01:34 +0100

    Sequential implementation added for speed up analysis

---
+++ MergeSort/SEQ/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#PBS -N AP
+#PBS -l walltime=119:00
+#PBS -l nodes=1:r641:ppn=32
+#PBS -q mei
+
+module load gcc/5.3.0
+
+cd MergeSort/SEQ
+
+rm AP.*
+make clean
+make
+
+mkdir times
+
+SIZE=512
+while [ $SIZE -lt 2049 ]; do
+    NUM=0
+    while [ $NUM -lt 15 ]; do
+        ./mergeSortSeq $SIZE >> times/mergesort_$SIZE.txt
+        let NUM=NUM+1
+    done
+    let SIZE=$SIZE*2
+done
