72aca325c293621e5b622109a987e42f00ec3234
jcm300 <zeclmartins@gmail.com>
2019-05-28 10:41:41 +0100

    changed script for Stencil

--- Stencil/run.sh
+++ Stencil/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 #PBS -N AP
-#PBS -l walltime=20:00
+#PBS -l walltime=90:00
 #PBS -l nodes=2:r641:ppn=32
 #PBS -q mei
 
@@ -14,12 +14,18 @@ make
 
 mkdir times
 
-OMP_NUM_THREADS=2
-while [ $OMP_NUM_THREADS -lt 65 ]; do
-    NUM=0
-	while [ $NUM -lt 15 ]; do
-		mpirun --map-by node --oversubscribe -np $OMP_NUM_THREADS bin/a.out >> times/mpi_res_stencil.txt
-        let NUM=NUM+1
+SIZE=512
+while [ $SIZE -lt 2049 ]; do
+    sed "s/#define M_SIZE.*/#define M_SIZE $SIZE/g" -i src/matrix_utils.hpp
+
+    OMP_NUM_THREADS=2
+    while [ $OMP_NUM_THREADS -lt 65 ]; do
+        NUM=0
+        while [ $NUM -lt 15 ]; do
+            mpirun --map-by node --oversubscribe -np $OMP_NUM_THREADS bin/a.out >> times/mpi_res_stencil_$SIZE.txt
+            let NUM=NUM+1
+        done
+        let OMP_NUM_THREADS=$OMP_NUM_THREADS*2
     done
-    let OMP_NUM_THREADS=$OMP_NUM_THREADS*2
+    let SIZE=$SIZE*2
 done
--- Stencil/src/matrix_utils.hpp
+++ Stencil/src/matrix_utils.hpp
@@ -5,7 +5,7 @@
 #define M_SIZE 895
 #define STENCIL_P 4
 
-#define ITERATIONS 256
+#define ITERATIONS 512
 void initiateMask(double *C);
 void initiateMatrix(double m[M_SIZE][M_SIZE]);
 void printResults(double m[M_SIZE][M_SIZE]);
