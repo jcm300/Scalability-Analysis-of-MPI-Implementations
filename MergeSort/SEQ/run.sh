47925bf395bfefb4d2613680be4de0622902f68f
MQuaresma <miguelquaresma97@gmail.com>
2019-05-29 07:35:28 +0100

    Fix cluster submission script

--- MergeSort/SEQ/run.sh
+++ MergeSort/SEQ/run.sh
@@ -19,7 +19,7 @@ SIZE=512
 while [ $SIZE -lt 2049 ]; do
     NUM=0
     while [ $NUM -lt 15 ]; do
-        ./mergeSortSeq $SIZE >> times/mergesort_$SIZE.txt
+        (time ./mergeSortSeq $SIZE) &> times/mergesort_$SIZE.txt
         let NUM=NUM+1
     done
     let SIZE=$SIZE*2
